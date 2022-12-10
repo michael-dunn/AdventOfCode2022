4a9a983066f919f630406c9cd748428f31a316ed
Michael Dunn <mikeydunn12@gmail.com>
2022-12-10 10:24:10 -0700

    Day 9

---
+++ Day9Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var fileData = File.ReadAllText("input.txt").Split(Environment.NewLine).Select(a => a.Split(' '));
+
+        (int x, int y) head = (0, 0);
+        (int x, int y) tail = (0, 0);
+
+        HashSet<(int, int)> result = new HashSet<(int, int)>();
+
+        result.Add(tail);
+
+        foreach (var action in fileData)
+        {
+            int movements = int.Parse(action[1]);
+            for (int i = 0; i < movements; i++)
+            {
+                switch (action[0])
+                {
+                    case "R":
+                        head = (head.x + 1, head.y);
+                        break;
+                    case "U":
+                        head = (head.x, head.y + 1);
+                        break;
+                    case "L":
+                        head = (head.x - 1, head.y);
+                        break;
+                    case "D":
+                        head = (head.x, head.y - 1);
+                        break;
+                }
+                if (IsTailTouchingHead(head, tail))
+                    continue;
+
+                tail = GetNewTailPosition(head, tail);
+                result.Add(tail);
+            }
+        }
+
+        Console.WriteLine(result.Count);
+    }
+
+    public static bool IsTailTouchingHead((int x, int y) head, (int x, int y) tail)
+    {
+        return Math.Abs(head.x - tail.x) < 2 && Math.Abs(head.y - tail.y) < 2;
+    }
+
+    public static (int, int) GetNewTailPosition((int x, int y) head, (int x, int y) tail)
+    {
+        if (head.x == tail.x)
+            return (tail.x, tail.y + ((head.y - tail.y) > 0 ? 1 : -1));
+        if (head.y == tail.y)
+            return (tail.x + ((head.x - tail.x) > 0 ? 1 : -1), tail.y);
+
+        return (tail.x + ((head.x - tail.x) > 0 ? 1 : -1), tail.y + ((head.y - tail.y) > 0 ? 1 : -1));
+    }
+}
---
+++ Day9Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var fileData = File.ReadAllText("input.txt").Split(Environment.NewLine).Select(a => a.Split(' '));
+
+        (int x, int y) head = (0, 0);
+        List<(int x, int y)> tails = new List<(int, int)>()
+        {
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+        };
+
+        HashSet<(int, int)> result = new HashSet<(int, int)>();
+
+        result.Add(tails[8]);
+
+        foreach (var action in fileData)
+        {
+            int movements = int.Parse(action[1]);
+            for (int i = 0; i < movements; i++)
+            {
+                switch (action[0])
+                {
+                    case "R":
+                        head = (head.x + 1, head.y);
+                        break;
+                    case "U":
+                        head = (head.x, head.y + 1);
+                        break;
+                    case "L":
+                        head = (head.x - 1, head.y);
+                        break;
+                    case "D":
+                        head = (head.x, head.y - 1);
+                        break;
+                }
+
+                if (IsTailTouchingHead(head, tails[0]))
+                    continue;
+
+                tails[0] = GetNewTailPosition(head, tails[0]);
+
+                for (int j = 1; j < tails.Count; j++)
+                {
+                    if (IsTailTouchingHead(tails[j - 1], tails[j]))
+                        continue;
+
+                    tails[j] = GetNewTailPosition(tails[j - 1], tails[j]);
+                }
+
+                result.Add(tails[8]);
+            }
+        }
+
+        Console.WriteLine(result.Count);
+    }
+
+    public static bool IsTailTouchingHead((int x, int y) head, (int x, int y) tail)
+    {
+        return Math.Abs(head.x - tail.x) < 2 && Math.Abs(head.y - tail.y) < 2;
+    }
+
+    public static (int, int) GetNewTailPosition((int x, int y) head, (int x, int y) tail)
+    {
+        if (head.x == tail.x)
+            return (tail.x, tail.y + ((head.y - tail.y) > 0 ? 1 : -1));
+        if (head.y == tail.y)
+            return (tail.x + ((head.x - tail.x) > 0 ? 1 : -1), tail.y);
+
+        return (tail.x + ((head.x - tail.x) > 0 ? 1 : -1), tail.y + ((head.y - tail.y) > 0 ? 1 : -1));
+    }
+}
